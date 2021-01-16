8694e92a2e11fda61ba8e5a4f9a3f66ca4afe814
Oleh-Khomenko <olehhomenko12@gmail.com>
2021-01-16 15:54:04 +0200

    .

--- TestAI/test.py
+++ TestAI/test.py
@@ -1,24 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from tqdm import tqdm
 
-def activate(x):
-    if x >= 0:
-        return 1
-    else:
-        return 0
+random.seed(1)
 
 
-inp = [[random.randint(-10, 10), random.randint(-10, 10)] for i in range(300)]
+def sigmoid(x):
+    return 1 / (1 + np.exp(x))
 
-out_neuron = [-0.3, 0.3, -0.3 * 2]
 
-for i in inp:
-    out = np.dot(out_neuron, i)
+class Neuron:
+    def __init__(self, weights, bias=0):
+        self.weights = weights
+        self.bias = bias
 
-    if activate(out):
-        plt.scatter(*i, color='green', s=1)
-    else:
-        plt.scatter(*i, color='blue', s=1)
+    def feed_forward(self, inp):
+        out = np.dot(inp, self.weights) + self.bias
+        return sigmoid(out)
 
-plt.show()
+
+class NeuralNetwork:
+    def __init__(self, hidden_counts, out_count):
+        self.hidden_layers = [[Neuron([random.random(), random.random()])
+                               for _ in range(hidden_counts[j])] for j in range(len(hidden_counts))]
+        self.out_neurons = [Neuron([random.random(), random.random()]) for _ in range(out_count)]
+        self.lamb = 0.01
+
+    def feed_forward(self, inp):
+        inputs = inp
+        for layer in self.hidden_layers:
+            res = []
+            for neuron in layer:
+                res.append(neuron.feed_forward(inputs))
+            inputs = res
+
+        out = []
+        for neuron in self.out_neurons:
+            out.append(neuron.feed_forward(inputs))
+
+        return out
+
+    def back_propagation(self, out_neuron, out):
+        """count of out neurons = 1"""
+
+        e = out - out_neuron
+        grad = 0
+
+    def train(self, inp, out):
+        pass
