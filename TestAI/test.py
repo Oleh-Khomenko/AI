fc77158d8c428968f10f3f352fad82458e88716a
Oleh-Khomenko <olehhomenko12@gmail.com>
2021-01-16 14:42:58 +0200

    .

--- TestAI/test.py
+++ TestAI/test.py
@@ -1,49 +1,24 @@
 import numpy as np
-from random import random, seed
+import matplotlib.pyplot as plt
+import random
 
-seed(1)
-
-
-def sigmoid(x, der=False):
-    if der:
-        return sigmoid(x) * (1 - sigmoid(x))
-    return 1 / (1 + np.exp(-x))
-
-
-class Neuron:
-    def __init__(self, weight, bias=0):
-        self.weight = weight
-        self.bias = bias
-
-    def feedforward(self, inp):
-        total = np.dot(self.weight, inp) + self.bias
-        return sigmoid(total)
-
-    def __str__(self):
-        return f'{self.weight}'
-
-    def __repr__(self):
-        return f'w: {self.weight}'
+def activate(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
 
 
-class NeuralNetwork:
-    def __init__(self, input_count, hidden_count, out_count):
-        input_layer = [Neuron(random()) for _ in range(input_count)]
-        hidden_layer = [Neuron(random()) for _ in range(hidden_count)]
-        out_layer = [Neuron(random()) for _ in range(out_count)]
+inp = [[random.randint(-10, 10), random.randint(-10, 10)] for i in range(300)]
 
-        self.network = [input_layer, hidden_layer, out_layer]
+out_neuron = [-0.3, 0.3, -0.3 * 2]
 
-    def activate(self):
-        for i in range(1, len(self.network)):
-            prev_layer = self.network[i - 1]
-            current_layer = self.network
+for i in inp:
+    out = np.dot(out_neuron, i)
 
+    if activate(out):
+        plt.scatter(*i, color='green', s=1)
+    else:
+        plt.scatter(*i, color='blue', s=1)
 
-
-    def __str__(self):
-        return '{}\n{}\n{}\n'.format(*self.network)
-
-
-network = NeuralNetwork(2, 1, 2)
-print(network)
+plt.show()
